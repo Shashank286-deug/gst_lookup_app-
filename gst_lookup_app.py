83b3a9537c88a84d8dd165583126709b29401625
Shashank <mrunique060110@gmail.com>
2025-04-16 12:10:10 +0530

    Update gst_lookup_app.py

--- gst_lookup_app.py
+++ gst_lookup_app.py
@@ -5,14 +5,14 @@ from io import BytesIO
 import re
 
 # Brave API Key
-GOOGLE_API_KEY= "AIzaSyA8CgZ39m5oDOE8dqdNR45AYTGlX9_4cgY"
+BRAVE_API_KEY="BSAEVojmmWmY2wnNwzDfRk9JOyfx5em"
 
 @st.cache_data(show_spinner=False)
 def search_gst_with_brave(name):
     query = f"{name} gst number"
     headers = {
         "Accept": "application/json",
-        "X-Subscription-Token": GOOGLE_API_KEY
+        "X-Subscription-Token": BRAVE_API_KEY
     }
     params = {"q": query, "count": 3}
     try:
